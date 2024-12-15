9631393fcc61e0f6349e9d4fdf5ded7250419bda
Saksham-garg <sakshamgarg142@hotmail.com>
2024-12-15 20:40:00 +0530

    text animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,13 +9,13 @@ import {
   BeautifulTimelineItem as TimelineItem,
 } from "react-beautiful-timeline";
 import "react-beautiful-timeline/dist/style.css";
-import Image from "next/image";
+import { GradualSpacing } from "./GradualSpacing/GradualSpacing";
 
 const Hero = () => {
   return (
     <MaxWidthWrapper className="flex flex-col items-center justify-center gap-6 md:gap-10 text-center h-full z-10">
       <h1 className="max-w-4xl md:mb-4 font-bold text-5xl md:text-6xl lg:text-7xl font-raleway">
-        Ace your <span className="">Projects</span> with us
+        <GradualSpacing text="Ace your Projects with us" />
       </h1>
       <p className="mt-5 max-w-prose sm:text-lg text-white">
         Unlock your academic potential with expert assistance. We're your secret
@@ -131,4 +131,4 @@ const Hero = () => {
   );
 };
 
-export default AppWrap(MotionWrap(Hero, "home h-full"), "home", "h-screen");
+export default AppWrap(Hero, "home", "h-screen");
